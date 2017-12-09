53a951296301e00e4a5c62db64697284e4f4b0a1
Pankaj Parkar <parkarpankaj3@gmail.com>
2017-12-09 21:10:51 +0530

    Loaded users from json

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -3,13 +3,23 @@
     angular.module('user-portal')
     .controller('HomeController', HomeController);
     
-    HomeController.$inject = [];
+    HomeController.$inject = ['UserService'];
 
-    function HomeController(){
+    function HomeController(userService){
         var home = this;
 
+        //methods
+        home.$onInit = $onInit;
+
+        function getUsers(){
+            userService.getUsers().then(function(users){
+                debugger
+                home.users = users;
+            });
+        }
+
         function $onInit(){
-            console.log(main);
+            getUsers();
         }
     }
 
--- app/services/user.service.js
+++ app/services/user.service.js
@@ -14,14 +14,27 @@
         userService.getUserById = getUserById;
 
         function getUsers(){
+            debugger
+            return $http.get('/api/users.json')
+                .then(unWrapData)
+                .catch(catchError);
         }
 
         function saveUser(){
-            
+            //modify local data here
         }
 
         function getUserById(){
-            
+            //retrieve from localStorage and lookup by id
+        }
+
+        function catchError(response){
+            console.log(response)
+            return response.data;
+        }
+
+        function unWrapData(response){
+            return response.data;
         }
     }
 
