7316473b12486df9875f6abe41951ecc52c1251b
Pankaj Parkar <parkarpankaj3@gmail.com>
2017-12-11 02:23:23 +0530

    Added user-edit component basic html

--- app/user-edit/user-edit.controller.js
+++ app/user-edit/user-edit.controller.js
@@ -4,13 +4,24 @@
     angular.module('user-portal')
     .controller('UserEditController', UserEditController);
     
-    UserEditController.$inject = [];
+    UserEditController.$inject = ['UserService'];
 
-    function UserEditController(){
+    function UserEditController(userService){
         var userEdit = this;
 
         //methods
         userEdit.$onInit = $onInit;
+        userEdit.getDepartment = getDepartment;
+        userEdit.save = save;
+
+        function getDepartment(){
+            //TODO: 
+        }
+
+        function save(){
+            //TODO: 
+            userService.save();
+        }
 
         function $onInit(){
             console.log(userEdit);
